43150adf6608176a12fe446334c758c90d553a53
ThovenhairWorrior <wswm2007@naver.com>
2023-04-12 23:32:05 +0900

    4-12_plus_sol

--- TIL-2023.04/pr2023-4-12/pgn의_배수_고르기.js
+++ TIL-2023.04/pr2023-4-12/pgn의_배수_고르기.js
@@ -1,5 +1,20 @@
 // 정수 n과 정수 배열 numlist가 매개변수로 주어질 때, numlist에서 n의 배수가 아닌 수들을 제거한 배열을 return하도록 solution 함수를 완성해주세요.
 
+// for문을 사용하지 않은 풀이
+
 function solution(n, numlist) {
     return [...numlist].filter((el)=> (el) % n === 0);
+}
+
+
+// for문을 사용한 풀이
+
+function solution(n, numlist) {
+    let answer = [];
+    for (let i of numlist) {
+        if (i % n === 0) {
+            answer.push(i)
+        }
+    }
+    return answer;
 }
--- TIL-2023.04/pr2023-4-12/pg숫자_찾기.js
+++ TIL-2023.04/pr2023-4-12/pg숫자_찾기.js
@@ -1,5 +1,7 @@
 // 정수 num과 k가 매개변수로 주어질 때, num을 이루는 숫자 중에 k가 있으면 num의 그 숫자가 있는 자리 수를 return하고 없으면 -1을 return 하도록 solution 함수를 완성해보세요.
 
+
+// for문을 사용하는 경우
 function solution(num, k) {
     num = [...num + ""]
     for (let i of num) {
@@ -8,4 +10,14 @@ function solution(num, k) {
         } 
     }
     return -1
+}
+
+
+// for문을 사용하지 않은 경우 -> 마지막 논리 연산자 부분 정답 참고
+
+function solution(num, k) {
+    return num.toString()
+              .split("")
+              .map((el) => Number(el))
+              .indexOf(k) + 1 || -1  // + 1 || -1 이부분이 이해가 가지 않는다.
 }
