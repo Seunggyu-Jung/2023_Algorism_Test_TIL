b5177c63e7d919164fee02171f3181b21cbc4196
ThovenhairWorrior <wswm2007@naver.com>
2023-04-13 23:06:24 +0900

    4-13TIL

---
+++ TIL-2023.04/pr2023-4-13/pg자릿수_더하기.js
@@ -0,0 +1,9 @@
+// 정수 n이 매개변수로 주어질 때 n의 각 자리 숫자의 합을 return하도록 solution 함수를 완성해주세요
+
+// for문을 사용하지 않은 풀이
+
+function solution(n) {
+    return n.toString()
+            .split("")
+            .reduce((a,c) => a + parseInt(c) ,0);
+}
