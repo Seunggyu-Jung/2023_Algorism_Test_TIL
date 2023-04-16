079b7ee505e26cc66170d229df03678bf2647b32
ThovenhairWorrior <wswm2007@naver.com>
2023-04-16 22:00:55 +0900

    4-16TIL

---
+++ TIL-2023.04/pr2023-4-16/pg잘라서_배열로_저장하기.js
@@ -0,0 +1,32 @@
+// 문자열 my_str과 n이 매개변수로 주어질 때, my_str을 길이 n씩 잘라서 저장한 배열을 return하도록 solution 함수를 완성해주세요.
+
+
+// 처음 풀이한 풀이
+
+function solution(my_str, n) {
+    my_str = my_str.split("");
+    let answer = [];
+    let STR = ""
+    for(let i of my_str) {
+        STR += i;
+        if (STR.length === n) {
+            answer.push(STR)
+            STR = ""
+        }
+    }
+    if (STR.length != 0) {
+        answer.push(STR)
+    }
+    return answer;
+}
+
+
+// 좀 더 깔끔하고 간단한 풀이
+
+function solution(my_str, n) {
+    let answer = [];
+    for (let i = 0; i < my_str.length ; i += n) {
+        answer.push(my_str.slice(0+i, n +i))
+    }
+    return answer
+}
