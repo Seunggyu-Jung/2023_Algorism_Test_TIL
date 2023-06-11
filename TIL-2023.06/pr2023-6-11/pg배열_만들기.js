bbf4dea92369c04b5c464798252449cc4ca9f484
Daniel <wswm2007@naver.com>
2023-06-11 23:26:22 +0900

    6-11TIL

---
+++ TIL-2023.06/pr2023-6-11/pg배열_만들기.js
@@ -0,0 +1,21 @@
+// 정수 n과 k가 주어졌을 때, 1 이상 n이하의 정수 중에서 k의 배수를 오름차순으로 저장한 배열을 return 하는 solution 함수를 완성해 주세요.
+
+// k의 배수만 뽑음
+function solution(n, k) {
+  let answer = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % k === 0) {
+      answer.push(i);
+    }
+  }
+  return answer;
+}
+
+// k의 배수를 만듦
+function solution(n, k) {
+  let answer = [];
+  for (let i = k; i <= n; i += k) {
+    answer.push(i);
+  }
+  return answer;
+}
