2039ba4464aac08fbbb30e8b00c4fac6eb0c9ba9
Daniel <wswm2007@naver.com>
2023-09-05 23:57:52 +0900

    알고리즘 테스트 Lv1 9-5TIL

--- TIL-2023.09/pr2023-9-5/pg실패율.js
+++ TIL-2023.09/pr2023-9-5/pg실패율.js
@@ -6,16 +6,26 @@
 // 스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수 / 스테이지에 도달한 플레이어 수
 // 전체 스테이지의 개수 N, 게임을 이용하는 사용자가 현재 멈춰있는 스테이지의 번호가 담긴 배열 stages가 매개변수로 주어질 때, 실패율이 높은 스테이지부터 내림차순으로 스테이지의 번호가 담겨있는 배열을 return 하도록 solution 함수를 완성하라.
 
-// 9월 2일 미완성 : 실패율까지 구함
+// 9월 5일 미완료
 function solution(N, stages) {
+  let result = [];
   let answer = 0;
+  let shellow = [];
   let za = 0;
   let set = [];
   for (let i = 1; i <= N; i++) {
-    stages.map((v, _) => (v === i ? (answer += 1) : (answer += 0)));
+    stages.map((v, index) => (v === i ? (answer += 1) : (answer += 0)));
     set.push(answer / (stages.length - za));
+    shellow.push(answer / (stages.length - za));
     za += answer;
     answer = 0;
   }
-  return set;
+  set.sort((a, b) => b - a);
+  for (let j = 0; j < set.length; j++) {
+    if (shellow.includes(set[j])) {
+      result.push(shellow.indexOf(set[j]) + 1);
+      set.splice(j, 1, '');
+    }
+  }
+  return result;
 }
