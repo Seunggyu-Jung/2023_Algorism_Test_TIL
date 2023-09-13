36b22f7a36d694f895daf8f32b10b26a33fadf2c
Daniel <wswm2007@naver.com>
2023-09-13 17:48:56 +0900

    알고리즘 테스트 Lv1 9-13TIL

---
+++ TIL-2023.09/pr2023-9-13/pg둘만의_암호.js
@@ -0,0 +1,134 @@
+// 두 문자열 s와 skip, 그리고 자연수 index가 주어질 때, 다음 규칙에 따라 문자열을 만들려 합니다. 암호의 규칙은 다음과 같습니다.
+// 문자열 s의 각 알파벳을 index만큼 뒤의 알파벳으로 바꿔줍니다.
+// index만큼의 뒤의 알파벳이 z를 넘어갈 경우 다시 a로 돌아갑니다.
+// skip에 있는 알파벳은 제외하고 건너뜁니다.
+// 예를 들어 s = "aukks", skip = "wbqd", index = 5일 때, a에서 5만큼 뒤에 있는 알파벳은 f지만 [b, c, d, e, f]에서 'b'와 'd'는 skip에 포함되므로 세지 않습니다. 따라서 'b', 'd'를 제외하고 'a'에서 5만큼 뒤에 있는 알파벳은 [c, e, f, g, h] 순서에 의해 'h'가 됩니다. 나머지 "ukks" 또한 위 규칙대로 바꾸면 "appy"가 되며 결과는 "happy"가 됩니다.
+// 두 문자열 s와 skip, 그리고 자연수 index가 매개변수로 주어질 때 위 규칙대로 s를 변환한 결과를 return하도록 solution 함수를 완성해주세요.
+
+// 본인 풀이 : 미리 알파벳 배열을 넉넉하게 만들어서 인덱스가 넘어가지 않도록 범위 설정
+function solution(s, skip, index) {
+  let answer = '';
+  let alpha = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+  ];
+  alpha = alpha.filter((v, _) => !skip.includes(v));
+  for (let i of s) {
+    answer += alpha[alpha.indexOf(i) + index];
+  }
+  return answer;
+}
+
+// 보다 간단한 풀이 : 알파벳 배열의 길이로 나누어 나머지를 구하면 알파벳 배열이 더 간단해진다.
+function solution(s, skip, index) {
+  let answer = '';
+  let alpha = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+  ];
+  alpha = alpha.filter((v, _) => !skip.includes(v));
+  return s
+    .split('')
+    .map((v, i) => alpha[(alpha.indexOf(v) + index) % alpha.length])
+    .join('');
+}
